29daed503f5e1b18443796b6018b816960ecdfa5
millatchowdhury <millatchowdhury@gmail.com>
2018-10-17 09:13:05 +0600

    learn php

---
+++ array_function.php
@@ -0,0 +1,38 @@
+ 
+
+1.	    array_search();
+2.	    array_walk($arr, "my_function");
+3.	    array_column($arr, 'last_name', 'id');
+    Multidimensional array ar modde same key ar value show korbe
+4.	    array_combine($name, $age); 
+    Associative_array te value gulo ke combine kore show kore
+5.	    array_map();
+6.	    in_array();if(in_array($text, $arr))(both)
+7.	    array_change_key_case($arr, CASE_UPPER);
+8.	    array_change_key_case($arr, CASE_LOWER);
+9.	    array_count_yalues($name);  value return kore (both)
+10.	array diff($arr, $arr2); (both)
+11.	array_diff_assoc($array_one, $array_two); (both)
+12.	array_diff_key($array_one, $array_two); (Associative)
+13.	array_intersect($array one, $array two); (both)
+14.	array_intersect_assoc($array_one, $array_two ); (both)
+15.	array_keys(); 
+16.	array_merge();
+17.	array_multisort(); sort_asc, sort_desc(both)
+18.	array_pad(); $arr,5,"blue" (both)
+19.	array_pop(); last value delete korar jonno (both)
+20.	array_push(); value add korar jonno (both)
+21.	array_product(); value gulo k gun kore(both)
+22.	array_replace();(both)
+23.	array_reverese();  (both)
+24.	array_shift(); first element ke remove korar jonno (both)
+25.	array_slice(); nirdisto value hote print kora (both))
+26.	array_sum(); jog korar jonno (use both array)
+27.	array_unique(); duplicate value remove kore dai (both)
+28.	array_unshift(); element add korar jonno(both)
+29.	array_values(); just value print korar jonno(both)
+30.	arsort(), asort(); (Associative)
+31.	compact(); variable ke array te rupantor kore
+32.	current(), next(), current(), prev(), end(); (both)
+33.	key(), ksort(), krsort(); key() diye current position nirnoy kora hoy.
+34.	shuffle();(both)
---
+++ structure.php
@@ -0,0 +1,23 @@
+ 
+echo
+print
+variable
+constant
+operator
+var_dump(), gettype()
+conditional statement
+loop
+function
+difference of echo and print, 
+strlen(),strrev(),strpos(),str_replace(),str_word_count()
+strtoupper, strtolower, ucfirst, ucwords
+variable scop
+array, array sorting
+superglobal variable
+filehandaling
+file upload
+session
+cookie
+explode, implode
+include,require
+form validation with checkbox, radio, select>option
